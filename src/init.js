68970df4a51daa847052c9083f79d369219ce397
Schmitzenbergh <rob-smit@outlook.com>
2018-03-27 21:44:27 +0200

    -Test for pull request -Removed Comment

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ process.stdout.write( '\n' + " initialize Src ");
 //module.exports = pathbridge = pathhue + './bridge/';
 //module.exports = pathlight = pathhue + './light/';
 //module.exports = pathsensor = pathhue + './sensor/';
-//
+
 //module.exports = require( pathbridge + './init.js');
 //module.exports = require( pathlight + './init.js');
 //module.exports = require( pathsensor + './init.js');
